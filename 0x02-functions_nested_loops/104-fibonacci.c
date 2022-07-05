43ff7af7a263215591cd55b7e1d808e1612fe764
alexandra <lourdesmboya@gmail.com>
2022-07-05 15:33:23 +0300

    Doing corrections for 0x02*/104_fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,35 +1,56 @@
+/*
+ * File: 104-fibonacci.c
+ * Auth: Lourdes Moya
+ */
+
 #include <stdio.h>
 
 /**
- * main - prints fibonnaci numbers
- *Auth: Lourdes Mboya
- * Description: prints from 1 to 98
- * Return: Always(0) Success
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ *        1 and 2, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int num1, num2, i, n;
-	int next_num;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
 
-	n = 98;
-
-	for (i = 1; i <= n; i++)
+	for (count = 0; count < 92; count++)
 	{
-		if (i == 98)
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		half1 = fib1_half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
-			printf("%d, ", num2);
+			half1 += 1;
+			half2 %= 10000000000;
 		}
-		else
-		{
-			printf("%d, ", num2);
-			next_num = num1 + num2;
-			num1 = num2;
-			num2 = next_num;
-		}
+
+		printf("%lu%lu", half1, half2);
+		if (count != 98)
+			printf(", ");
+
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
 	}
 	printf("\n");
-
 	return (0);
 }
-
